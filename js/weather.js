59d4d0b1feddfeb2d9a245500ef9cd3b63a1dda5
wakass222 <will-z@live.com.au>
2017-02-04 22:46:05 +1100

    Added Geolocation to weather and now shows location

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+  });
+});
+
+function loadWeather(location, woeid) {
   $.simpleWeather({
-    location: 'Austin, TX',
-    woeid: '',
+    location: location,
+    woeid: woeid,
     unit: 'c',
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+        html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
 
       $("#weather").html(html);
     },
@@ -12,4 +19,4 @@ $(document).ready(function() {
       $("#weather").html('<p>'+error+'</p>');
     }
   });
-});
+}
